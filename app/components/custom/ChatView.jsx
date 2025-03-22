6c1aefe251b8549cb61165904ddd7d5d67913319
Nikhil <nikhilgupta932003@gmail.com>
2025-03-23 02:23:26 +0530

    added ai in the site now giving response

--- app/components/custom/ChatView.jsx
+++ app/components/custom/ChatView.jsx
@@ -9,8 +9,12 @@ import { useParams } from 'next/navigation'
 import React, { useContext, useEffect, useState } from 'react'
 import Image from 'next/image';
 import { FaLink } from 'react-icons/fa';
-import { ArrowRight } from 'lucide-react';
+import { ArrowRight, Loader2Icon } from 'lucide-react';
 import Lookup from '@/app/data/Lookup';
+import axios from 'axios';
+import Promt from '@/app/data/Promt';
+// import { run } from '.../configs/AiModel.jsx'; 
+import Prompt from '@/app/data/Prompt';
 
 const Chatview = () => {
      const {id}=useParams();
@@ -18,9 +22,12 @@ const Chatview = () => {
      const {userDetail,setUserDetail}=useContext(UserDetailContext)
      const {messages,setMessages}=useContext(MessagesContext)
      const [userInput,setUserInput]=useState();
+     const [loading,setLoading]=useState(false);
 
     
-
+     useEffect(()=>{
+        id&&GetWorkspaceData();
+      },[id])
 
      const GetWorkspaceData=async()=>{
         const result=await convex.query(api.workSpace.GetWorkspace,{
@@ -32,22 +39,63 @@ const Chatview = () => {
      }
 
      useEffect(()=>{
-        id&&GetWorkspaceData();
-      },[id])
+        if(messages?.length>0){
+            const role=messages[messages?.length-1].role;
+
+            if(role=='user'){
+                GetAiResponse();
+            }
+        }
+     },[messages])
+
+
+     const GetAiResponse=async()=>{
+        setLoading(true)
+        const PROMPT=JSON.stringify(messages)+Prompt.CHAT_PROMPT;
+        const result=await axios.post('/api/ai-chat',{
+            prompt:PROMPT
+        },{
+            headers: { 'Content-Type': 'application/json' }
+        });
+        console.log(result.data.result);
+
+        setMessages(prev=>[...prev,{
+            role:'ai',
+            content:result.data.result
+        }])
+        setLoading(false)
+     }
+
+     const onGenerate=(input)=>{
+        setMessages(prev=>[...prev,{
+            role:'user',
+            content:input
+        }])
+     }
+
+    
 
   return (
     <div className='relative h-[85vh] flex flex-col'>
         <div className='flex-1 overflow-y-scroll'>
             {(messages || [])?.map((msg,index)=>(
-                <div key={index} className='p-3 rounded-lg mb-2 flex gap-2 items-start'
+                <div key={index} className='p-3 rounded-lg mb-2 flex gap-2 items-start leading-7'
                 style={{
                     backgroundColor:Color.CHAT_BACKGROUND
                 }}>
                     {msg?.role==='user'&& (<Image src={userDetail?.picture} alt='userImage' width={35} height={35}
                         className='rounded-full'/>)}
                     <h2>{msg.content}</h2>
+                   
                 </div>
             ))}
+            {loading&& <div className='p-3 rounded-lg mb-2 flex gap-2 items-start'
+            style={{
+                backgroundColor:Color.CHAT_BACKGROUND
+            }}>
+                        <Loader2Icon className='animate-spin'/>
+                        <h2>generating response...</h2>
+                    </div>}
         </div>
 
         {/* input section  */}
---
+++ app/data/Prompt.jsx
@@ -0,0 +1,11 @@
+import decent from "dedent";
+
+export default{
+    CHAT_PROMPT:decent`
+    'You are a AI Assistance and experience in react development.
+    Guidelines:
+    -Tell user what are you building
+    -response less than 15 lines
+    -skip code example and commentary'
+    `
+}
